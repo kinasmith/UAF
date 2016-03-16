639364ece9f5d7fd19e23b10cbe60eaa4a37e04f
Kina Smith <kina.smith@gmail.com>
2016-03-16 11:46:57 -0800

    did some minor pcb work and did some data analysis

---
+++ data/analysis.R
@@ -0,0 +1,15 @@
+datafram_data$time = as.POSIXct(datafram_data$X1457737135, origin="1970-01-01")
+datafram_data$time
+
+setwd("~/Documents/Projects/UAF//uaf_dendrometer/data/test/")
+getwd()
+data = data.frame(read.csv("2016.03.16/12.CSV"))
+names(data) <- c("sensor", "temp", "voltage", "time")
+attach(data)
+diff = matrix(NA, nrow = (length(time)-1))
+aa = (length(time)-1)
+  for(i in 1:aa){
+    diff[i] <- time[i+1] - time[i]
+  }
+
+plot(diff)
