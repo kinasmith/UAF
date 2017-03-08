bd69a56ec604bf980ae0e11ed38b38fa2a60ec93
Kina Smith <kina.smith@gmail.com>
2017-03-07 17:09:34 -0900

    testing thermocouple stability

--- sap-flux/sapflux-v20/src/tc_test/tc_test.cpp
+++ sap-flux/sapflux-v20/src/tc_test/tc_test.cpp
@@ -5,6 +5,8 @@
 #define TC1_CS 7
 #define TC2_CS 6
 #define TC3_CS 5
+#define HEATER_EN 4
+#define LED 9
 
 #define SERIAL_EN //Comment this out to remove Serial comms and save a few kb's of space
 
@@ -18,7 +20,7 @@
 #define DEBUGFlush();
 #endif
 
-MAX31856 tc1(TC1_CS, T_TYPE, CUTOFF_60HZ, AVG_SEL_1SAMP, CMODE_AUTO, ONESHOT_OFF); //constant conversion mode
+MAX31856 tc1(TC1_CS, T_TYPE, CUTOFF_60HZ, AVG_SEL_1SAMP, CMODE_OFF, ONESHOT_ON); //constant conversion mode
 MAX31856 tc2(TC2_CS, T_TYPE, CUTOFF_60HZ, AVG_SEL_4SAMP, CMODE_OFF, ONESHOT_ON); //one shot mode
 MAX31856 tc3(TC3_CS, T_TYPE, CUTOFF_60HZ, AVG_SEL_4SAMP, CMODE_OFF, ONESHOT_ON); //one shot mode
 
@@ -26,16 +28,23 @@ struct Measurement {
 	double tc1 = 0;
 	double tc2 = 0;
 	double tc3 = 0;
-	double internal = 0;
+	double tc1_int = 0;
+	double tc2_int = 0;
+	double tc3_int = 0;
 };
+
+int cnt = 0;
 
 
 void setup() {
-  pinMode(10, OUTPUT);
 	Serial.begin(9600);
+	pinMode(HEATER_EN, OUTPUT);
+	pinMode(LED, OUTPUT);
+	tc1.begin();
+	tc2.begin();
+	tc3.begin();
   DEBUGln("START--");
 	delay(100);
-	DEBUGln("START 2 --");
 }
 
 void loop() {
@@ -49,11 +58,25 @@ void loop() {
   thisMeasurement.tc1 = tc1.getExternal();
   thisMeasurement.tc2 = tc2.getExternal();
   thisMeasurement.tc3 = tc3.getExternal();
-  thisMeasurement.internal = tc1.getInternal();
-  DEBUG("- ");
+  thisMeasurement.tc1_int = tc1.getInternal();
+	thisMeasurement.tc2_int = tc2.getInternal();
+	thisMeasurement.tc3_int = tc3.getInternal();
   DEBUG(thisMeasurement.tc1); DEBUG(", ");
   DEBUG(thisMeasurement.tc2); DEBUG(", ");
   DEBUG(thisMeasurement.tc3); DEBUG(", ");
-  DEBUG(thisMeasurement.internal); DEBUGln();
+  DEBUG(thisMeasurement.tc1_int); DEBUG(", ");
+	DEBUG(thisMeasurement.tc2_int); DEBUG(", ");
+	DEBUG(thisMeasurement.tc3_int); DEBUG(", ");
+	DEBUGln();
+	if(cnt < 5) {
+		digitalWrite(HEATER_EN, HIGH);
+		digitalWrite(LED, HIGH);
+	} else {
+		digitalWrite(HEATER_EN, LOW);
+		digitalWrite(LED, LOW);
+	}
+	if(cnt > 60) cnt = 0;
+	cnt++;
 	delay(1000);
+
 }
